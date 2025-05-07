c8efeb3fca233004d3759e4c097d4e8728d1d4fa
LuanQS2 <165271965+LuanQS2@users.noreply.github.com>
2025-05-07 08:40:48 -0300

    New page projects

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from "../footer/footer.component";
-import { HeaderComponent } from "../header/header.component";
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
